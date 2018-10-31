4ad9919510308809356b9be7662f03b4c450fc58
Albin Winkelmann <albin-w@hotmail.com>
2018-10-31 20:15:37 +0100

    Gitignore fix

--- src/Secret.h
+++
@@ -1,8 +0,0 @@
-const char* ssid = "..........";
-const char* password = "..........";
-
-/* MQTT */
-#define Broker              ""
-#define Port                1883
-#define MQTTUsername        ""
-#define MQTTPassword        ""
