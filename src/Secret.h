9cbed2c6e578dea7472a618d2a3e70fbdf7d04e0
Albin Winkelmann <albin-w@hotmail.com>
2018-10-31 20:16:47 +0100

    Added empty Secret.h

---
+++ src/Secret.h
@@ -0,0 +1,8 @@
+const char* ssid = "..........";
+const char* password = "..........";
+
+/* MQTT - Settings */
+#define Broker              ""
+#define Port                1883
+#define MQTTUsername        ""
+#define MQTTPassword        ""
